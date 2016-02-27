a7ab8512c3e58735e2bf6bb848333c94f61125a4
soumya suansia <soumya.suansia@gmail.com>
2016-02-27 23:55:03 +0530

    linear search code

---
+++ py3/ExampleSet_01/linearsearch.py
@@ -0,0 +1,24 @@
+
+numberofelement = int(input("How many element to process: "))
+arr = []
+
+for n in range(numberofelement):
+    print (n)
+    arr.append(int(input("Enter number: ")))
+    
+searchelement = int(input("Enter the number you want to search: "))
+
+def l_search(arr, element):
+    for i in range(0,len(arr)):
+        if arr[i] == element:
+            return i
+    else:
+        return -1
+
+re = l_search(arr, searchelement)
+
+if ( re == -1):
+    print ("The element not available in the list.")
+elif (re >= 0):
+    print ("The element found at: %s th position." % re) 
+    
